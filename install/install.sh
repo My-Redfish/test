a6f7d2f7c65d80ba7d1bee7ace19470d205e546d
mark <marktsai0316@gmail.com>
2026-07-30 16:44:02 +0800

    Modify install.sh for bmcsvc and Yafuflash
    Updated installation script to reflect new directory names and added support for Yafuflash.

--- install/install.sh
+++ install/install.sh
@@ -5,14 +5,22 @@ TMP="${PWD}"
 echo "$TMP"
 
 
-sudo mkdir -p /usr/local/lib/bmcsvr
+sudo mkdir -p /usr/local/lib/bmcsvc
 
-sudo cp -a "$TMP/bmcsvr/." /usr/local/lib/bmcsvr/
+sudo cp -a "$TMP/bmcsvc/." /usr/local/lib/bmcsvc/
 
 sudo ln -sf \
-    /usr/local/lib/bmcsvr/bmcsvr-cli.sh \
+    /usr/local/lib/bmcsvc/bmcsvc-cli.sh \
     /usr/local/bin/bmcsvc
 
+sudo mkdir -p /usr/local/lib/Yafuflash
+
+sudo cp -a "$TMP/Yafuflash/." /usr/local/lib/Yafuflash/
+
+sudo ln -sf \
+    /usr/local/lib/Yafuflash/Linux_x86_64/Yafuflash \
+    /usr/local/bin/Yafuflash
+    
 rm -rf "$TMP"
 
 echo "Installed."
